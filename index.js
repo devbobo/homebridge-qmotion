1351e6991758f4ad70fc26a8b9eb0a3822ea3863
David Parry <david@smugmug.com>
2016-02-10 22:16:05 +1100

    refactor

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ function QMotionPlatform(log, config, api) {
     var self = this;
 
     this.api = api;
-    this.accessories = [];
-    this.configured = [];
+    this.accessories = {};
     this.log = log;
 
     this.api.on('didFinishLaunching', function() {
@@ -28,18 +27,13 @@ function QMotionPlatform(log, config, api) {
         client.on("found", function(device) {
             device.on("blind", function(blind) {
                 var uuid = UUIDGen.generate(blind.addr);
+                var accessory = self.accessories[uuid];
 
-                if (self.configured.indexOf(uuid) == -1) {
+                if (accessory === undefined) {
                     self.addAccessory(blind);
                 }
                 else {
-                    for (var index in self.accessories) {
-                        if (self.accessories[index].UUID == uuid) {
-                            self.log("Online: %s [%s]", self.accessories[index].displayName, blind.addr);
-                            self.initAccessory(self.accessories[index], blind);
-                            break;
-                        }
-                    }
+                    self.accessories[uuid] = new QMotionAccessory(self.log, accessory, blind);
                 }
             });
         });
@@ -47,35 +41,32 @@ function QMotionPlatform(log, config, api) {
 }
 
 QMotionPlatform.prototype.addAccessory = function(blind) {
-    var self = this;
-
     this.log("Found: %s [%s]", blind.name, blind.addr);
 
     var accessory = new PlatformAccessory(blind.name, UUIDGen.generate(blind.addr));
     accessory.addService(Service.WindowCovering);
-    this.initAccessory(accessory, blind);
+    this.accessories[accessory.UUID] = new QMotionAccessory(this.log, accessory, blind);
 
-    self.configured.push(accessory.UUID);
-    self.accessories.push(accessory);
-    self.api.registerPlatformAccessories("homebridge-qmotion", "QMotion", [accessory]);
+    this.api.registerPlatformAccessories("homebridge-qmotion", "QMotion", [accessory]);
 }
 
 QMotionPlatform.prototype.configureAccessory = function(accessory) {
-    this.configured.push(accessory.UUID);
-    this.accessories.push(accessory);
+    this.accessories[accessory.UUID] = accessory;
 }
 
-QMotionPlatform.prototype.initAccessory = function(accessory, blind) {
+function QMotionAccessory(log, accessory, blind) {
     var self = this;
 
-    accessory.blind = blind;
+    this.accessory = accessory;
+    this.blind = blind;
+    this.log = log;
 
-    accessory.blind.on('currentPosition', function(blind){
+    this.blind.on('currentPosition', function(blind){
         console.log("currentPosition", blind.state.currentPosition);
         accessory.getService(Service.WindowCovering).getCharacteristic(Characteristic.CurrentPosition).setValue(blind.state.currentPosition);
     });
 
-    accessory.blind.on('positionState', function(blind){
+    this.blind.on('positionState', function(blind){
         accessory.getService(Service.WindowCovering).getCharacteristic(Characteristic.PositionState).setValue(blind.state.positionState);
     });
 
@@ -83,31 +74,35 @@ QMotionPlatform.prototype.initAccessory = function(accessory, blind) {
 
     service
         .getCharacteristic(Characteristic.CurrentPosition)
-        .on('get', function(callback) {callback(null, accessory.blind.state.currentPosition)})
-        .setValue(accessory.blind.state.currentPosition);
+        .on('get', function(callback) {callback(null, self.blind.state.currentPosition)})
+        .setValue(self.blind.state.currentPosition);
 
     service
         .getCharacteristic(Characteristic.TargetPosition)
         .setProps({ minStep: 25 })
-        .on('get', function(callback) {callback(null, accessory.blind.state.targetPosition)})
-        .on('set', function(value, callback) {self.setTargetPosition(accessory, value, callback)});
+        .on('get', function(callback) {callback(null, self.blind.state.targetPosition)})
+        .on('set', function(value, callback) {self.setTargetPosition(value, callback)});
 
     service.getCharacteristic(Characteristic.PositionState)
-        .on('get', function(callback) {callback(null, accessory.blind.state.positionState)})
-        .setValue(accessory.blind.state.positionState);
+        .on('get', function(callback) {callback(null, self.blind.state.positionState)})
+        .setValue(self.blind.state.positionState);
+
+    accessory.updateReachability(true);
 }
 
-QMotionPlatform.prototype.setTargetPosition = function(accessory, value, callback){
-    this.log("%s - Setting target position: %s", accessory.displayName, value);
+QMotionAccessory.prototype.setTargetPosition = function(value, callback){
+    this.log("%s - Setting target position: %s", this.accessory.displayName, value);
 
-    accessory.blind.move(value, function(position) {
+    var self = this;
+
+    this.blind.move(value, function(position) {
         if (position == null) {
             callback(new Error("Invalid Target Position"), false);
             return;
         }
 
         // send the command twice in case the blind was already moving
-        accessory.blind.move(value, function(position) {
+        self.blind.move(value, function(position) {
             callback();
         });
     });
